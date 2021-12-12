747a4a7d6df581541812ff590b1c0bf5c1bebb87
gedon1st <kopytin0707@mail.ru>
2021-12-12 14:39:31 +0400

    new sql and index page

--- src/main/resources/mysql.sql
+++ src/main/resources/mysql.sql
@@ -141,11 +141,77 @@ create procedure createUser(_login varchar(128), _password varchar(128), _email 
 insert into users (login, password, email, role_id)
 values (_login, _password, _email, (SELECT ID FROM role WHERE type = 'USER'));
 
+create procedure userGet(in _id int)
+select u.id, u.login, u.password, r.type as role
+from users u
+         join role r on r.id = u.role_id
+where u.id = _id;
+
+create procedure removeUser(_login varchar(128))
+delete from users where login = _login;
+
+create procedure findUser(_login varchar(128))
+SELECT u.id, u.login, u.email, r.type
+FROM users u
+         join role r on u.role_id = r.id
+where u.login = _login;
+
+create procedure allUsers() SELECT * FROM users;
+
+
+
+
+
 create procedure createProduct(_user_id int, _image varchar(128), _name varchar(128), _description varchar(256), _price int, _date date)
 insert into product (user_id, image, name, description, price, date)
 values (_user_id, _image, _name, _description, _price, _date);
 
+create procedure removeProduct(_product_id int)
+delete from product where id = _product_id;
+
+
+create procedure allProduct() SELECT * FROM product;
+
+
 create procedure createItem(_name varchar(128), _image varchar(128), _date date, _description varchar(128), _user_id integer)
-insert into product (name, image, date, description, user_id)
+insert into item (name, image, date, description, user_id)
 values (_name, _image, _date, _description, _user_id);
+
+create procedure removeItem(_item_id int)
+delete from item where id = _item_id;
+
+create procedure allItem() SELECT * FROM item;
+
+
+
+create procedure createComment(_user_id int, _date date, _message varchar(128), _item_id int)
+insert into comment (user_id,  date, message, item_id)
+values (_user_id, _date, _message, _item_id);
+
+create procedure removeComment(_item_id int)
+delete from comment where id = _comment_id;
+
+
+
+
+create procedure createCategory(_type varchar(64))
+insert into category (type) values (_type);
+
+create procedure removeCategory(_category_id int)
+delete from category where id = _category_id;
+
+
+create procedure allCategory() SELECT * FROM category;
+
+
+create procedure createUserInfo(_name varchar(128), _lastname varchar(128), _phone varchar(64), _image varchar(128),_birthday date)
+insert into user_info (name, lastName, phone, image, birthday)
+values (_name, _lastname, _phone, _image, _birthday);
+
+
+
+
+
+
+
 
