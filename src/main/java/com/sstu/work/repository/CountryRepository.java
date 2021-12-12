1f816298cb3b82151d0a1e828fe885081b1bc1a4
mitlg <mitlg7@gmail.com>
2021-12-12 22:24:50 +0300

    COUNTRY

--- src/main/java/com/sstu/work/repository/CountryRepository.java
+++ src/main/java/com/sstu/work/repository/CountryRepository.java
@@ -1,4 +1,15 @@
 package com.sstu.work.repository;
 
+import com.sstu.work.model.Country;
+
+import java.util.List;
+
 public class CountryRepository {
+
+    public Country getById(){
+        return null;
+    }
+    public List<Country> getAll(){
+        return null;
+    }
 }
