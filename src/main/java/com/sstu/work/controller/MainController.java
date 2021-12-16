2003e83e0899e61d72206d1669ad6d20585ef551
miku0221 <mitlg@gmail.com>
2021-12-16 13:28:21 +0400

    ыы

--- src/main/java/com/sstu/work/controller/MainController.java
+++ src/main/java/com/sstu/work/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.sstu.work.controller;
 
 import com.sstu.work.model.Product;
+import com.sstu.work.service.CategoryService;
 import com.sstu.work.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,30 +11,44 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
-import java.util.Collection;
 import java.util.List;
-import java.util.ListIterator;
+import java.util.stream.Collectors;
 
 @Controller
 public class MainController {
     @Autowired
     ProductService productService;
-
+    @Autowired
+    CategoryService categoryService;
 
     @GetMapping
-    public String a(Principal user, Model model, @RequestParam(required = false) String search){
-        if(search == null){
+    public String a(Principal user, Model model,
+                    @RequestParam(required = false) String search,
+                    @RequestParam(required = false) String cat) {
+        if (search == null && cat == null) {
             model.addAttribute("products", productService.getAllProducts());
-        }else {
-            List<Product> products = productService.searchProducts(search);
-            if(!CollectionUtils.isEmpty(products)){
-                model.addAttribute("products", productService.searchProducts(search));
-            }else {
-                model.addAttribute("notfound", true);
-            }
-            model.addAttribute("search", search);
-        }
+        } else {
+            List<Product> products = productService.getAllProducts();
 
+            if(search != null){
+                products = productService.searchProducts(search);
+                if (!CollectionUtils.isEmpty(products)) {
+                    ;
+                } else {
+                    model.addAttribute("notfound", true);
+                }
+                model.addAttribute("search", search);
+            }
+            if (cat != null) {
+                model.addAttribute("catId",Long.parseLong( cat));
+                products = products.stream().filter(product -> product.getCategory().getId().equals(Long.parseLong(cat))).collect(Collectors.toList());
+            }
+            model.addAttribute("products", products);
+            model.addAttribute("search", search);
+
+        }
+        System.out.println(categoryService.getAll());
+        model.addAttribute("cats", categoryService.getAll());
         return "index";
     }
 }
